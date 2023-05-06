6f3eebd481ff8ff21b1e2de7fa7d6df6778d6ddf
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-05-06 07:14:57 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,20 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id]) #コメントを投稿されるユーザー見つける
-    comment = current_user.@book.new(book_comment_params) #ログインユーザーがコメントを作成する
-    comment.book_id = book.id
-    byebug
+    comment = current_user.book_comments.new(book_comment_params) #ログインユーザーがコメントを作成する
+    comment.book_id = @book.id #新規コメントと、投稿されるユーザーを一致させる
     comment.save #コメントを保存する
     @user = @book.user
-    render 'books/show'
-    #comment.book_id = book.id
+    #@book_comment = BookComment.new
+    #render 'books/show'
+    render :comments
   end
 
   def destroy
     @book = Book.find(params[:book_id])
+    #@book_comment = BookComment.new
     BookComment.find(params[:id]).destroy
-    comment.destroy
+    #comment.destroy
     #renderしたときに@bookのデータがないため定義
     render :comments #render先にjsファイルを指定
   end
